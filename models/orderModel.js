359317cac28e74aafa7a7f2b8b378b1c388d9b3b
ifthikar2267 <seyedahamedifthikar40@gmail.com>
2025-03-16 22:00:06 +0530

    district

--- models/orderModel.js
+++ models/orderModel.js
@@ -30,6 +30,7 @@ const orderSchema = new mongoose.Schema(
       lastName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
+      district: { type: String, required: true },
       state: { type: String, required: true },
       zipCode: { type: String, required: true },
       country: { type: String, required: true },
